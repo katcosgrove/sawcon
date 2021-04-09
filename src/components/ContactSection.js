aaba038d91076e8fef5614709b0e626e76951271
Stackbit Code Editor <projects@stackbit.com>
2021-04-09 20:42:11 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -45,7 +45,7 @@ export default class ContactSection extends React.Component {
                       contacted.</label>
                   </div>
                   <div className="form-submit">
-                    <button type="submit" className="button">Get In Touch</button>
+                    <button type="submit" className="button">Submit</button>
                   </div>
                 </form>
               </div>
